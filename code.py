dc79a5c6564e12c96191effcc65765b31185fcff
Pulkit Agarwal <apulkit21@gmail.com>
2020-08-06 20:57:27 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,24 +2,36 @@
 #for x in f:
  # print(x)
 import re
-filename = "SBLAYOUT.txt"
+filename = "inflaout.txt"
 outputstring =''
 
 
 def DataType(clause):
-	if 'X' in clause:
-		if 'XX' in clause or '(' in clause :
-			return 'String'
-		return 'Integer'
-	elif 'Z' in clause:
-		return 'decimal'
+	if 'X' in clause and '(' in clause :
+		getva = clause.replace('X','')
+		getvb = getva.rstrip('.')
+		return 'char' + getvb
+	elif 'X' in clause:
+		countx = str(clause.count('X'))
+		
+		return 'char(' + countx + ')'
+	elif 'Z' in clause or 'S' in clause or 'V' in clause:
+		return 'decimal(10,4)'
+	else:
+		return 'String'
+	
 	
 for line in open(filename, 'r'):
 
 	if "PIC" in line:
-		k = re.sub(' +', ' ',line)
-		li = list(k.split(" ")) 
-		datatype = DataType(li[4])
-		outputstring = outputstring + li[2] + ':' + datatype + '|'
+		commentline = line
+		if ' *' not in commentline:
+			final = commentline
+#			print(final)
+			k = re.sub(' +', ' ',final)
+			li = list(k.split(" "))
+#			print(li[4])
+			datatype = DataType(li[4])
+			outputstring = outputstring + li[2] + ':' + datatype + '|'
 print(outputstring)
-	
+		
